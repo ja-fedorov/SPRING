41b716f3d495a49087ac00f3844ec0844eb33363
Yaroslav Fedorov <89410042+ja-fedorov@users.noreply.github.com>
2022-09-22 16:48:18 +0300

    Add files via upload

---
+++ SpringMVC_3/src/main/java/ua/fedorov/spring/config/MySpringMvcDispatcherServletInitializer.java
@@ -0,0 +1,24 @@
+package ua.fedorov.spring.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import org.springframework.web.servlet.support.AbstractDispatcherServletInitializer;
+
+
+//Заменяет web.xml
+public class MySpringMvcDispatcherServletInitializer
+        extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
