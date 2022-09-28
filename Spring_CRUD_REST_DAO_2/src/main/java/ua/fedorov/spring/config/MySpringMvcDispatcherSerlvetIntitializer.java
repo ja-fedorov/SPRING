57c89dec6855e08fbbf9c237f1b75be6c79c7a87
Yaroslav Fedorov <89410042+ja-fedorov@users.noreply.github.com>
2022-09-28 10:07:45 +0300

    Add files via upload

---
+++ Spring_CRUD_REST_DAO_2/src/main/java/ua/fedorov/spring/config/MySpringMvcDispatcherSerlvetIntitializer.java
@@ -0,0 +1,21 @@
+package ua.fedorov.spring.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMvcDispatcherSerlvetIntitializer
+        extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
---
+++ Spring_CRUD_REST_DAO_2/src/main/java/ua/fedorov/spring/controllers/PeopleController.java
@@ -0,0 +1,43 @@
+package ua.fedorov.spring.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ua.fedorov.spring.dao.PersonDAO;
+import ua.fedorov.spring.models.Person;
+
+@Controller
+@RequestMapping("/people")
+public class PeopleController {
+
+    private PersonDAO personDAO;
+
+    public PeopleController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @GetMapping()
+    public String index(Model model){
+            model.addAttribute("people", personDAO.index());
+        return "people/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show (@PathVariable("id") int id, Model model) {
+            model.addAttribute("person", personDAO.show(id));
+        return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person) {
+        return "people/new";
+    }
+
+    @PostMapping
+    public String create (@ModelAttribute("person") Person person) {
+        personDAO.save(person);
+        return "redirect:/people";
+    }
+
+}
